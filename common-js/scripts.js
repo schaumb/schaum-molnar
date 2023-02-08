026960c5ee173e748e2fd44c7bd338c74314af54
Bela Schaum <schaumb@users.noreply.github.com>
2023-02-08 15:37:38 +0000

    Add filled modal & read URL params, update bootstrap

--- common-js/scripts.js
+++ common-js/scripts.js
@@ -18,6 +18,16 @@
 		$('#main-menu').removeClass('visible');
 	});
 	
+	$.urlParam = function (name) {
+		var results = new RegExp('[\?&]' + name + '=([^&#]*)')
+						  .exec(window.location.search);
+	
+		return (results !== null) ? results[1] || 0 : false;
+	}
+
+	if ($.urlParam('filled')) {
+		$('#filledModal').modal()
+	}
 })(jQuery);
 
 function doScrolling(element, duration) { 
